fb173e115fd2768b68a38c97c1952f32065781ff
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2022-09-10 11:33:43 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+import fetchCountries from './fetchCountries';
+const debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
+
+const inputElem = document.querySelector('input#search-box');
+//console.log(inputElem);
+
+inputElem.addEventListener('input', debounce(fetchCountries, DEBOUNCE_DELAY));
